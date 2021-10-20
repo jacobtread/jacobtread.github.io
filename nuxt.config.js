32ee6c05162e57384756a8ce23e6db2ab50976dc
Jacobtread <jacobtread@gmail.com>
2021-10-20 17:18:41 +1300

    Fixed meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,6 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
       {
         property: 'og:type',
